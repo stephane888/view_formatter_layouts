8f125c97b458d2bbbd72dee315166a4cdc2ece2d
stephane888 <kksasteph888@gmail.com>
2021-08-02 02:25:29 +0100

    Maj

---
+++ src/Plugin/views/style/viewFormatterLayoutsDefault.php
@@ -0,0 +1,113 @@
+<?php
+namespace Drupal\view_formatter_layouts\Plugin\views\style;
+
+use Drupal\core\form\FormStateInterface;
+use Drupal\views\Plugin\views\style\StylePluginBase;
+use Drupal\view_formatter_layouts\ViewFormatterLayouts;
+
+/**
+ * Style plugin to render a list of years and months
+ * in reverse chronological order linked to content.
+ *
+ * @ingroup views_style_plugins
+ *
+ * @ViewsStyle(
+ *   id = "viewformatterlayoutsdefault",
+ *   title = @Translation("view Layout Default"),
+ *   help = @Translation("Render default model"),
+ *   theme = "view_formatter_layouts_default",
+ *   display_types = { "normal" }
+ * )
+ */
+class viewFormatterLayoutsDefault extends StylePluginBase {
+
+  /**
+   * Does the style plugin for itself support to add fields to it's output.
+   *
+   * @var bool
+   */
+  protected $usesFields = TRUE;
+
+  /**
+   * Does the style plugin allows to use style plugins.
+   *
+   * @var bool
+   */
+  protected $usesRowPlugin = TRUE;
+
+  /**
+   *
+   * {@inheritdoc}
+   */
+  protected function defineOptions()
+  {
+    $options = parent::defineOptions();
+    $options['view_layouts_fields'] = [
+      'default' => NULL
+    ];
+    $options['view_layouts_options'] = [
+      'default' => NULL
+    ];
+    return $options;
+  }
+
+  /**
+   *
+   * {@inheritdoc}
+   */
+  public function buildOptionsForm(&$form, FormStateInterface $form_state)
+  {
+    parent::buildOptionsForm($form, $form_state);
+    if (isset($form['grouping'])) {
+      unset($form['grouping']);
+      $labels = $this->CustomLabels($this->displayHandler->getFieldLabels(TRUE));
+      // listes des champs.
+      $form['view_layouts_fields'] = [
+        '#type' => 'hidden',
+        '#default_value' => ViewFormatterLayouts::serialise($this->defaultOptions())
+      ];
+      /**
+       * add section
+       */
+      $form['view_layouts_options'] = array(
+        '#type' => 'details',
+        '#title' => 'Mappes les champs ci-dessous.',
+        "#tree" => true,
+        '#open' => true
+      );
+      foreach ($labels as $key => $label) {
+        $form['view_layouts_options'][$key] = [
+          '#type' => 'select',
+          '#title' => $label,
+          '#options' => $this->defaultOptions(),
+          // '#required' => TRUE,
+          '#default_value' => (isset($this->options['view_layouts_options'][$key])) ? $this->options['view_layouts_options'][$key] : '',
+          '#empty_value' => ''
+          // '#description' => $this->t('Select the layout that will be used to display "' . $label . '"')
+        ];
+      }
+    }
+  }
+
+  /**
+   * Fournis le model à utiliser
+   */
+  protected function defaultOptions($model = "cards")
+  {
+    $options = [];
+    if ($model == 'cards') {
+      $options = [
+        'card_title_field' => 'Titre layout',
+        'card_content_field' => 'contenus layout',
+        'card_image_field' => 'images layout',
+        'card_button_field' => 'buton layout'
+      ];
+    }
+    return $options;
+  }
+
+  protected function CustomLabels($data)
+  {
+    return \json_decode(json_encode($data), true);
+  }
+}
---
+++ src/ViewFormatterLayouts.php
@@ -0,0 +1,76 @@
+<?php
+namespace Drupal\view_formatter_layouts;
+
+use Symfony\Component\Serializer\Serializer;
+use Drupal\views\ViewExecutable;
+use Drupal\views\Entity\View;
+use Drupal\views\ResultRow;
+use Drupal\views\Plugin\views\field\Custom as viewCustomField;
+use Drupal\views\Render\ViewsRenderPipelineMarkup;
+use Drupal\view_formatter_layouts\Plugin\views\style\VflLamaisonsaintgobainComments;
+
+class ViewFormatterLayouts {
+
+	/**
+	 * Returns the theme hook definition information.
+	 */
+	public static function getThemeHooks() {
+		$hooks['view_formatter_layouts_default'] = [
+			'preprocess functions' => [
+				'template_preprocess_view_formatter_layouts_default'
+			],
+			'file' => 'view_formatter_layouts.theme.inc'
+		];
+		//
+		$hooks['vfl_lamaisonsaintgobain_comments'] = [
+			'preprocess functions' => [
+				'template_preprocess_vfl_lamaisonsaintgobain_comments'
+			],
+			'file' => 'vfl_lamaisonsaintgobain_comments.theme.inc'
+		];
+		return $hooks;
+	}
+
+	public static function serialise($data, $format = 'json') {
+		// $_Serializer = new Serializer();
+		// return $_Serializer->encode($data, $format);
+		return serialize($data);
+	}
+
+	public static function unserialize($data, $format = 'json') {
+		// $_Serializer = new Serializer();
+		// return $_Serializer->encode($data, $format);
+		return unserialize($data);
+	}
+
+	public static function getViewTitle(View $View, array &$variables) {
+		$variables["title"] = $View->label();
+	}
+
+	/**
+	 * Cette function est utile pour pouvoir suivre les classes et valider l'object
+	 *
+	 * @param VflLamaisonsaintgobainComments $plugin
+	 * @return \Drupal\view_formatter_layouts\Plugin\views\style\VflLamaisonsaintgobainComments
+	 */
+	public static function pluginVflLamaisonsaintgobainComments(VflLamaisonsaintgobainComments $plugin) {
+		return $plugin;
+	}
+
+	public static function ResultRow(ResultRow $ResultRow) {
+		//
+	}
+
+	public static function viewCustomField(viewCustomField $viewCustomField) {
+		// $viewCustomField->getValue($values)
+	}
+
+	public static function ViewsRenderPipelineMarkup(ViewsRenderPipelineMarkup $ViewsRenderPipelineMarkup) {
+		return $ViewsRenderPipelineMarkup->__toString();
+	}
+
+	public static function ViewExecutable(ViewExecutable $ViewExecutable, array &$variables) {
+		$variables['title'] = $ViewExecutable->display_handler->default_display->options['title'];
+	}
+}
+
